422c2b903695020230f0c7a3d2fe944ca18ed490
Aaron Langley <alangley345@gmail.com>
2022-04-08 09:29:17 -0400

    Update CalculateAge.cs

--- CalculateAge.cs
+++ CalculateAge.cs
@@ -1,10 +1,10 @@
 //Uses Jaguar's boiler plate to try below code and return a empty string is their is an exception.
 public string CalculateAge(string arg1){
     try{       
-        int age             = 0; 
+        int age         = 0; 
         string format   = "yyyyMMdd";
-        age = (DateTime.Today.Subtract(DateTime.ParseExact(arg1, format, null))).Days;
-        age= age / 365;  
+        age             = (DateTime.Today.Subtract(DateTime.ParseExact(arg1, format, null))).Days;
+        age             = age / 365;  
         return age.ToString();  
     }
     catch (Exception ex){
