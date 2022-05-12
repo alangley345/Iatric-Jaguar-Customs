7dffee12078e58695cb20011939440d8c0909368
Aaron Langley <alangley345@gmail.com>
2022-05-12 14:30:18 -0400

    Update ecw_obr28_to_pv152.cs

--- ecw_obr28_to_pv152.cs
+++ ecw_obr28_to_pv152.cs
@@ -1,17 +1,27 @@
-//function overwrites 2nd repeat in OBR-28 with contents of PV1-52
+//Uses Jaguar's boiler plate to try below code and return a empty string is their is an exception.
+public string obr28_pv152(string arg1 string arg2){
+    try{       
+        //function overwrites 2nd repeat in OBR-28 with contents of PV1-52
+        //arg1 = PV1-52
+        //arg2 = OBR-28
 
-//arg1 = PV1-52
-//arg2 = OBR-28
+        string newString = "";
+        arg2             = arg2 + "~~~";
+        string[] subVals = arg2.Split('~');
+        if (arg1 != (""))
+        {
+                newString = subVals[0] + "~" + arg1 + "~" + subVals[2] + "~" + subVals[3];
+        }
+        
+        else
+        {
+                newString = subVals[0] + "~" + subVals[1];
+        }
+        return newString.Trim('~')
 
-string newString = "";
-arg2             = arg2 + "~~~";
-string[] subVals = arg2.Split('~');
-if (arg1 != (""))
-{
-    newString = subVals[0] + "~" + arg1 + "~" + subVals[2] + "~" + subVals[3];
+    }
+
+    catch (Exception ex){
+        return string.Empty;
+    }
 }
-else
-{
-    newString = subVals[0] + "~" + subVals[1];
-}
-return newString.Trim('~')
