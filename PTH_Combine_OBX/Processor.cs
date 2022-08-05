c17e99dad7c03ba4d94b4cd214ff935671245b46
Aaron Langley <alangley345@gmail.com>
2022-08-05 09:24:32 -0400

    Added custom process I have been working on

---
+++ PTH_Combine_OBX/Processor.cs
@@ -0,0 +1,84 @@
+﻿using Iatric.EasyConnect.AddIns.Views;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CHMC.PTH
+{
+    [System.AddIn.AddIn("PTH_COMBINE_OBX", Description = "Concatenate all obx segments, separated with ~", Publisher = "Aaron Langley")]
+    public class Processor : AddInBase, ICustomProcessorAddInView
+    {
+
+        public bool Initialize(string debugFilePath, string parameters)
+        {
+            string str = this.InitializeBase(debugFilePath, parameters);
+            bool flag;
+            if (string.IsNullOrEmpty(str))
+            {
+                flag = true;
+            }
+            else
+            {
+                this.ErrorMessage = str;
+                flag = false;
+            }
+            return flag;
+        }
+
+        public DataStatus ProcessData(byte[] data, string tempFilePath)
+        {
+            HL72Message hl7msg = new HL72Message();
+            DataStatus dataStatus;
+            try
+            {
+                string message = this.UnicodeBytesToString(data);
+                hl7msg.Load(message);
+
+                List<string> segments = hl7msg.GetAllSegments();
+                string OBX15 = "";
+                string OBX1 = "";
+
+                foreach (string s in segments)
+                {
+                    if (s.Contains("OBX|1|"))
+                    {
+                        OBX1 += s;
+                    }
+                    if (s.Contains("OBX"))
+                    {
+                        string[] OBX = s.Split('|');
+                        OBX15 += OBX[5] + '~'; 
+                    }
+                }
+                string[] temp = OBX1.Split('|');
+                temp[5] = OBX15;
+
+                string j = string.Join("|", temp);
+               
+                data = Encoding.UTF8.GetBytes(j);
+                File.WriteAllBytes(tempFilePath, data);
+                dataStatus = DataStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                ProjectData.SetProjectError(ex);
+                ProjectData.ClearProjectError();
+                this.ErrorMessage = ex.ToString();
+                dataStatus = DataStatus.Error;   
+            }
+            return dataStatus;
+        }
+
+        private string UnicodeBytesToString(byte[] bytes) => Encoding.ASCII.GetString(bytes);
+
+        public bool ShutDown() => true;
+
+        public string ErrorMessage { [DebuggerNonUserCode] get; [DebuggerNonUserCode] set; }
+
+        public string FilterMessage { [DebuggerNonUserCode] get; [DebuggerNonUserCode] set; }
+    }
+}
---
+++ PTH_Combine_OBX/ReadMe.txt
@@ -0,0 +1,16 @@
+﻿============================
+Project Contents
+============================
+
+The following files should appear as part of this Visual C# Class Library project:
+
+ReadMe.txt
+                - This file.
+
+Processor.cs
+                - Class containing the entry point for your implementation.
+                - You may modify this file to suit your needs.
+
+HL72Message.cs
+                - A light-weight HL7 2.x message parser.
+                - You may modify this file to suit your needs.
